d6e72bb80e5d027fb168e6c13d95ea7dc1ef9d07
Alexandre CMC Souza <ale@alemacedo.com>
2023-07-13 14:22:14 -0300

    Debug: direction was wrong when user change browser tab.

--- src/components/gsap-fullscreen-plugin.jsx
+++ src/components/gsap-fullscreen-plugin.jsx
@@ -1,22 +1,33 @@
 "use client"
 import { useGesture } from '@use-gesture/react'
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { gsap } from "gsap";
 import { ScrollToPlugin } from 'gsap/all';
 gsap.registerPlugin(ScrollToPlugin);
 
 export default function FullScreenScroller({ children, options={}, className="", otherProps }) {
 
-
     const scrollAttempts = useRef(0);
-    const isScrolling = useRef(false);
     const currentPage = useRef(0);
+
+    const cPage = useCallback( ( page ) => {
+        if( page === undefined ) return currentPage.current;
+        currentPage.current = page;
+        console.log(  "current page: ", currentPage.current );
+    }, [currentPage] );
+
+
+    const isScrolling = useRef(false);
     const mainRef = useRef(null);
+
+    const childRef = useRef( new Array() );
+
     const markers = options.markers || false;
     const [ markerInfo, setMarketInfo ] = useState( "" );
 
     useGesture({
         onWheel: (state) => {
+            console.log( state.direction, state.down )
             scrollIfPossible(state);
         },
         onWheelEnd: (state) => {
@@ -59,6 +70,11 @@ export default function FullScreenScroller({ children, options={}, className="",
     function scrollIfPossible( state ) {
 
         scrollAttempts.current += 1;
+        if ( scrollAttempts.current !== 1 ) return;
+        if ( state.direction[1] === 0 && !state.dragging ) {
+            scrollAttempts.current = 0;
+            return
+        };
 
         let direction = state.direction[1] === 1 ? 1 : -1;
         direction = state.dragging ? direction * -1 : direction;
@@ -70,19 +86,20 @@ export default function FullScreenScroller({ children, options={}, className="",
             const scrollOptions = children.map( ( child ) => child.props.snapconfig || {} );
             const targets = gsap.utils.toArray( "*[snapconfig]" )
             const maxPage = targets.length - 1;
-            const nextPage = currentPage.current + direction >= maxPage
+
+            const nextPage =  cPage() + direction >= maxPage
                 ? maxPage
-                : currentPage.current + direction < 0
+                :  cPage() + direction < 0
                     ? 0
-                    : currentPage.current + direction;
+                    :  cPage() + direction;
             const wait = scrollOptions[ nextPage ]?.wait || 0.2;
 
-            if ( scrollAttempts.current !== 1 ) return;
-            if ( targets[ nextPage ] === undefined ) return;
-            if ( nextPage === currentPage.current ) return;
 
-            const duration = direction === 1 ? scrollOptions[currentPage.current]?.duration || 0.25 : scrollOptions[nextPage]?.duration || 0.25;
-            const ease = direction === 1 ? scrollOptions[ currentPage.current ]?.ease || "power0" : scrollOptions[ nextPage ]?.ease || "power0";
+            if ( targets[ nextPage ] === undefined ) return;
+            if ( nextPage ===  cPage() ) return;
+
+            const duration = direction === 1 ? scrollOptions[ cPage()]?.duration || 0.25 : scrollOptions[nextPage]?.duration || 0.25;
+            const ease = direction === 1 ? scrollOptions[  cPage() ]?.ease || "power0" : scrollOptions[ nextPage ]?.ease || "power0";
             gsap.to( window, {
                 duration: duration,
                 scrollTo: targets[ nextPage ].offsetTop,
@@ -96,7 +113,7 @@ export default function FullScreenScroller({ children, options={}, className="",
             function onComplete( nextPage, wait ) {
                 setTimeout( () => {
                     isScrolling.current = false;
-                    currentPage.current = nextPage;
+                    cPage( nextPage );
                 }, wait * 1000 )
             }
             function onUpdate( nextPage ) {
